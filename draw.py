13384407ff31bdf5b77175093fe25099aad25250
Nicholas Lau <nicholas.m.lau@gmail.com>
2015-11-02 18:19:22 -0800

    added input validation

--- draw.py
+++ draw.py
@@ -5,3 +5,11 @@ from submodules import shape
 if __name__ == '__main__':
   input = input.parseInput() # returns (shape, size, format)
   print input
+
+  if (input[0] == 'spiral'):
+    shape = shape.Spiral(input[1])
+    shape.name()
+  else:
+    print "Invalid Shape: " + input[0]
+    # shape = shape.Shape(input[1])
+    # shape.name()
--- submodules/input.py
+++ submodules/input.py
@@ -3,6 +3,13 @@ import optparse
 import argparse
 
 def parseInput():
+  input = parseOpts() # returns (shape, size, format)
+  isValidInput = validateInput(input)
+  if (isValidInput == False):
+    raise ValueError("Invalid Input: " + str(input))
+  return input
+
+def parseOpts():
   optionParser = optparse.OptionParser()
   optionParser.add_option('--shape', '-s', default="spiral")
   optionParser.add_option('--size', '-t', default="0")
@@ -10,3 +17,30 @@ def parseInput():
   options, arguments = optionParser.parse_args()
   print '%s %s %s' % (options.shape, options.size, options.format)
   return (options.shape, options.size, options.format)
+
+def parseArgs():
+  return 0
+  
+def validateInput(input):
+  # takes tuple (shape, size, format)
+  shape = input[0]
+  size = input[1]
+  format = input[2]
+
+  inputIsValid = True
+  # Validate size
+  if (int(size) < 0):
+    print "Invalid Size: " + size
+    inputIsValid = False
+  
+  # Validate shape
+  if (shape != 'spiral'): # || shape != Rectangle || shape != etc
+    print "Invalid Shape: " + shape
+    inputIsValid = False
+
+  # Validate format
+  if (format != 'ascii'): # || format != nvg || format != etc
+    print "Invalid Format: " + format
+    inputIsValid = False
+
+  return inputIsValid
