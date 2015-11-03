a47e9b743e1842ca590cb56f0a6b9dc87dc2f662
Nicholas Lau <nicholas.m.lau@gmail.com>
2015-11-02 18:51:27 -0800

    added basic shape (spiral)  module

--- submodules/shape.py
+++ submodules/shape.py
@@ -3,8 +3,16 @@
 class Shape():
   def draw(self):
     return 0
-  def area(self):
+  def name(self):
+    print ("Shape")
+  def __init__(self, size):
+    self.size = size
+
+class Spiral(Shape):
+  def draw(self):
     return 0
-  def __init__(self, width, height):
-    self.width = width
-    self.height = height
+  def name(self):
+    print ("Spiral, of size: " + self.size)
+  def __init__(self, size):
+    self.size = size
+
